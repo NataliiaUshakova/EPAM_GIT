6457e904aff2a0fa614d649687d4bf70cb1730ce
NataliiaUshakova <Nataliia_Ushakova@epam.com>
2018-05-07 15:37:48 +0300

    add one more line

--- src/EPAM_GIT.java
+++ src/EPAM_GIT.java
@@ -2,5 +2,6 @@ public class EPAM_GIT {
     public static void main(String[] args) {
         System.out.println("test");
         System.out.println("test2");
+        System.out.println("test3");
     }
 }
